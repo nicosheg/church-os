2efc3cb1b4452916e86e59e1923979f9c23dcf1b
Egwame Nicholas <nicosheg0123@gmail.com>
2026-07-17 14:52:03 +0100

    Create twilio.js

---
+++ utils/telephony/providers/twilio.js
@@ -0,0 +1,28 @@
+import twilio from 'twilio';
+
+export async function placeCall(member, escalate = false) {
+  const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
+  const twiml = `
+    <Response>
+      <Say voice="Polly.Joanna">
+        Hello ${member.first_name}, this is Grace from your church.
+        We missed seeing you recently and just wanted to check on you.
+      </Say>
+      <Gather input="speech dtmf" numDigits="1" action="/api/call/response?member_id=${member.id}" timeout="5">
+        <Say>If you would like us to pray with you, press 1. If you would like a visit, press 2.</Say>
+      </Gather>
+      <Say>Thank you, and God bless you. We look forward to seeing you soon.</Say>
+    </Response>
+  `;
+
+  const call = await client.calls.create({
+    twiml,
+    to: member.phone,
+    from: process.env.TWILIO_PHONE_NUMBER,
+    statusCallback: `/api/call/status?member_id=${member.id}`,
+    statusCallbackEvent: ['completed'],
+  });
+
+  return call.sid;
+}

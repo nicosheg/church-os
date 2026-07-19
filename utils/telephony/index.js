49a8761750939b935cef8ba021f8e9383169494f
Egwame Nicholas <nicosheg0123@gmail.com>
2026-07-19 08:48:21 +0100

    Update index.js

--- utils/telephony/index.js
+++ utils/telephony/index.js
@@ -1,10 +1,5 @@
-// This is the only place that knows which provider is used.
-// To switch, change the import below.
+import { placeCall } from './providers/whatsapp-local';
 
-import { placeCall } from './providers/twilio';
-
-export async function initiateFollowUpCall(member, escalate = false) {
-  // Here you could add logic to decide which provider to use
-  // based on member's country, cost, etc.
-  return await placeCall(member, escalate);
+export async function initiateFollowUpCall(member, escalate = false, customMessage = null) {
+  return await placeCall(member, escalate, customMessage);
 }
